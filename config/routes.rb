ca41c4874e6bead287e6a7bcc02444c18bd866ec
keisan0601 <angelica.actiloba@icloud.com>
2021-03-28 01:43:50 +0000

    Finish login-function

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   #ログイン機能
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
-  delete '/logout', to: 'sessions#delete'
+  delete '/logout', to: 'sessions#destroy'
   
   resources :users
   
