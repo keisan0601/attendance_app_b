5bc685e72946f9bfca30301fe2453d6346773aab
keisan0601 <angelica.actiloba@icloud.com>
2021-04-09 12:31:14 +0000

    Finish add-basic-info

--- db/schema.rb
+++ db/schema.rb
@@ -21,8 +21,8 @@ ActiveRecord::Schema.define(version: 20210331123736) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2021-03-30 23:00:00"
-    t.datetime "work_time", default: "2021-03-30 22:30:00"
+    t.datetime "basic_time", default: "2021-04-07 23:00:00"
+    t.datetime "work_time", default: "2021-04-07 22:30:00"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
